715b4083357d7bcfaed8f494f3da5b84e8c810ce
IgorRz9 <160083637+IgorRz9@users.noreply.github.com>
2024-09-20 13:50:46 -0300

    Add files via upload

---
+++ despesa.dart
@@ -0,0 +1,9 @@
+// modelos/despesa.dart
+class Despesa {
+  String descricao; 
+  DateTime data; 
+  double valor; 
+
+  // Construtor da classe Despesa
+  Despesa({required this.descricao, required this.data, required this.valor});
+}
