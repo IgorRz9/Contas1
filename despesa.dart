a849c03d31549f909614c3a707055ca4f6998461
IgorRz9 <160083637+IgorRz9@users.noreply.github.com>
2024-09-20 14:00:35 -0300

    Delete despesa.dart

--- despesa.dart
+++
@@ -1,9 +0,0 @@
-// modelos/despesa.dart
-class Despesa {
-  String descricao; 
-  DateTime data; 
-  double valor; 
-
-  // Construtor da classe Despesa
-  Despesa({required this.descricao, required this.data, required this.valor});
-}
